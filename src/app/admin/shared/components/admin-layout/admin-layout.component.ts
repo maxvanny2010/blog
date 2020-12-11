a879b16c767bce6c4cc7706cb8a9f389f2995b84
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-11 15:41:59 +0200

    6. admin-layout -> navbar(h1,ul,li(a))

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  logout(event: Event): void {
+    event.preventDefault();
+  }
 }
