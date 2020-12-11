b9aebddacbf4f43119df86cb006aa32950eb69a2
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-11 21:15:05 +0200

    9.admin form Gaurd canActive:[Guard] if this.auth.isAuthenticated() return true | else this.router.navigate(['/admin', 'dashboard'], this.router.navigate with queryParams: {authenticated: true}

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,6 +8,8 @@ import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AuthService} from './shared/services/auth.service';
+import {AuthGuard} from './shared/services/auth.guard';
+import {LoginGuard} from './shared/services/login.guard';
 
 @NgModule({
   declarations: [
@@ -23,17 +25,17 @@ import {AuthService} from './shared/services/auth.service';
       {
         path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
-          {path: 'login', component: LoginPageComponent},
-          {path: 'dashboard', component: DashboardPageComponent},
-          {path: 'create', component: CreatePageComponent},
-          {path: 'post/:id/edit', component: EditPageComponent},
+          {path: 'login', component: LoginPageComponent, canActivate: [LoginGuard]},
+          {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+          {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+          {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]},
         ]
       }
     ]),
     ReactiveFormsModule
   ],
   exports: [RouterModule],
-  providers: [AuthService]
+  providers: [AuthService, AuthGuard, LoginGuard]
 })
 export class AdminModule {
 
