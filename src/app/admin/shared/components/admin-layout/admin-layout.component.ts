061883c967681b9b4c4bc0e2f86700fc3f3d2ef8
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-11 15:46:53 +0200

    6. admin-layout -> logout.router.navigate.

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
@@ -15,5 +16,7 @@ export class AdminLayoutComponent implements OnInit {
 
   logout(event: Event): void {
     event.preventDefault();
+    this.router.navigate(['/admin', 'login']).then(() => {
+    });
   }
 }
