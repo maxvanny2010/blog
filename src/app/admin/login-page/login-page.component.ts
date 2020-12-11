2b147e85531fd713e017231ac4adbb77ca47edc8
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-11 16:54:03 +0200

    6. admin-layout -> login -> email|password->get data from form by user interface

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -26,9 +27,12 @@ export class LoginPageComponent implements OnInit {
   }
 
   submit(): void {
-    console.log(this.form);
     if (this.form.invalid) {
       return;
     }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
   }
 }
