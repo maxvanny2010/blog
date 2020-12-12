d9d753648fe2270b7e4882e2238ec06d0387b444
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-12 19:16:39 +0200

    24. home page all posts  *ngIf="post$ | async as posts; else loading"  @Input() post: Post;   <app-post      *ngFor="let post of posts"      [post]="post"    ></app-post>

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
+import {Post} from '../shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  post$: Observable<Post[]>;
 
-  constructor() { }
+  constructor(private postService: PostsService) {
+  }
 
   ngOnInit(): void {
+    this.post$ = this.postService.getAll();
   }
 
 }
