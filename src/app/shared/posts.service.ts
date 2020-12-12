b1888e73d43e909bd2def62288b2874ba82a0632
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-12 16:03:39 +0200

    18. admin dashboard-page getAll

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -22,4 +22,16 @@ export class PostsService {
           }
         ));
   }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any }) => {
+        return Object.keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }));
+      }));
+  }
 }
